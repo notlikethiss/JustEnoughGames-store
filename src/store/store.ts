24a64407f0cd295608515ad64c1813a9da8c687e
notlikethiss <notlikeethiss@gmail.com>
2023-05-14 12:02:09 +0300

    fetching fix

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
+import searchSlice from "./slices/searchSlice";
 
 export const store = configureStore({
 	reducer: {
-
+		search: searchSlice
 	}
 })
 
