838e130ef4930e5712fdfc4de74c5a18ac2cc5e3
notlikethiss <notlikeethiss@gmail.com>
2023-06-04 19:27:01 +0300

    routing fix

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
+import './notfound.style.scss'
+
 const NotFoundPage:React.FC = () => {
 
   return (
 
-	<div>NotFoundPage</div>
+	  <div className='notfound'>
+      <div className='notfound-container'>
+        <h1 className='notfound-text'>Страница не найдена</h1>
+        <img 
+        alt='emoji' 
+        className='search-image' 
+        src='https://em-content.zobj.net/thumbs/160/apple/118/pensive-face_1f614.png'
+        >
+        </img>
+      </div>
+    </div>
 
   )
 }
--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -4,13 +4,16 @@ import React from 'react'
 import Product from '../../components/Product/Product'
 
 //hooks
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 //data
 import { database } from '../../data/database'
 import NotFound from '../../components/Header/Search/NotFound'
+import NotFoundPage from '../NotFound/NotFoundPage'
 
 const ProductPage:React.FC = () => {
+
+  const nav = useNavigate()
 
   const { name } = useParams()
 
@@ -35,7 +38,7 @@ const ProductPage:React.FC = () => {
 					image={item.image}
 					filter={item.filter}
 				/>
-			)) : <NotFound />
+			)) : <NotFoundPage />
 		}
 	</div>
 
