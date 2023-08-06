a9bfe0487bce95cdffdf3b5fd6e76c2f54172848
notlikethiss <notlikeethiss@gmail.com>
2023-08-06 15:12:40 +0300

    removed favourites button

--- src/components/Header/ActionPanel/ActionPanel.tsx
+++ src/components/Header/ActionPanel/ActionPanel.tsx
@@ -13,11 +13,6 @@ const ActionPanel: React.FC<ICart> = ({ visibleCart, setVisibleCart }) => {
 	return (
 
 		<div className='right-side'>
-			<img
-				className='favorite'
-				alt='favorite'
-				src='https://media.discordapp.net/attachments/888505536457367552/1101961803648475166/icons8-heart-100.png'
-			></img>
 			<div onClick={() => visibleCart ? setVisibleCart(false) : setVisibleCart(true)} className='cart-panel'>
 				<div className='cart-counter'>{cartContainer.length === 0 ? null : cartContainer.length}</div>
 				<img
