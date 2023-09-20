26ebf2bb00a6b6400ad3f3b2a982a5a996aeb363
notlikethiss <notlikeethiss@gmail.com>
2023-09-20 13:25:25 +0300

    added margin on product card

--- src/components/Product/Product.tsx
+++ src/components/Product/Product.tsx
@@ -19,15 +19,16 @@ const Product: React.FC<ICardItem> = (product) => {
 	}, [product, dispatch])
 
 	return (
-
-		<div className='product-card'>
-			<div className='product-image' style={{ backgroundImage: `url(${product.image})` }} />
-			<div className='product-rightSide'>
-				<h1 className='product-name'>{product.name}</h1>
-				<p className='product-dev'>Разработчик: <br /> {product.developer}</p>
-				<p className='product-category'>Жанр: <br />{product.description}</p>
-				<span className='product-price'>{product.price} ₽</span>
-				<button onClick={cartHandler} className='product-button'>В корзину</button>
+		<div className='product-wrapper'>
+			<div className='product-card'>
+				<div className='product-image' style={{ backgroundImage: `url(${product.image})` }} />
+				<div className='product-rightSide'>
+					<h1 className='product-name'>{product.name}</h1>
+					<p className='product-dev'>Разработчик: <br /> {product.developer}</p>
+					<p className='product-category'>Жанр: <br />{product.description}</p>
+					<span className='product-price'>{product.price} ₽</span>
+					<button onClick={cartHandler} className='product-button'>В корзину</button>
+				</div>
 			</div>
 		</div>
 
